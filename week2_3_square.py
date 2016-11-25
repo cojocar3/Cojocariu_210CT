745fd4aa096e109a66457ed4196a3946f5c440ea
cojocar3 <catalin_andrei195@yahoo.com>
2016-11-25 14:09:25 +0000

    Update week2_3_square.py

--- week2_3_square.py
+++ week2_3_square.py
@@ -1,5 +1,7 @@
 def square(x):
+    last_s = None 
     for i in range(1,x+1):
         for j in range(1,x+1):
-            if j*j == i:
-                print (i)
+            if j*j == i :
+                last_s = i 
+    print (last_s)
